55c5c012dff9f7c5e5c5d048cd273db069f905e6
Deji <90336178+Dejihz@users.noreply.github.com>
2025-04-08 19:12:31 +0200

    fixed Context genre

--- 6.1Api/Entities/ContentGenre.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace project6._1Api.Entities
-{
-    public class ContentGenreModel
-    {
-        public int content_id { get; set; }
-        public int genre_id { get; set; }
-    }
-}
--- 6.1Api/Model/RefreshTokenRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace project6._1Api.Model
-{
-    public class RefreshTokenRequest
-    {
-        public string RefreshToken { get; set; }
-    }
-}
